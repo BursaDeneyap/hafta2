c90b9f6a760cc571ca3d828f8dafeca87a75fb39
M3li5 <magnezyum2009@gmail.com>
2026-07-14 12:44:20 +0300

    son

--- Melis.dart
+++ Melis.dart
@@ -1,17 +1,20 @@
+import 'dart:io';
 int topla(int sayi1, int sayi2) {
   return sayi1 + sayi2;
 }
 int kare(sayi3) {
     return sayi3 * sayi3;
 }
-String selamla(isim) {
-    return "Selamlar " + isim;
-}
+String selamla(isim) => "Selamlar " + isim;
 void main() {
-  var sayi1=12;
-  var sayi2=5;
-  var sayi3=25;
-  var isim= "Melis";
+  print("Toplanılacak ilk sayı giriniz: ");
+  var sayi1=int.parse(stdin.readLineSync()!);
+  print("Toplanılacak ikinci sayı giriniz: ");
+  var sayi2=int.parse(stdin.readLineSync()!);
+  print("Karesi alınacak sayı giriniz: ");
+  var sayi3=int.parse(stdin.readLineSync()!);
+  print("İsminizi giriniz: ");
+  var isim= stdin.readLineSync()!;
   print(topla(sayi1, sayi2));
   print(kare(sayi3));
   print(selamla(isim));
