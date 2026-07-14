642ab220cf20c213fd7810158acdf99a09bcbafc
M3li5 <magnezyum2009@gmail.com>
2026-07-14 11:33:45 +0300

    Fonksiyonlar

---
+++ Melis.dart
@@ -0,0 +1,18 @@
+int topla(int sayi1, int sayi2) {
+  return sayi1 + sayi2;
+}
+int kare(sayi3) {
+    return sayi3 * sayi3;
+}
+String selamla(isim) {
+    return "Selamlar " + isim;
+}
+void main() {
+  var sayi1=12;
+  var sayi2=5;
+  var sayi3=25;
+  var isim= "Melis";
+  print(topla(sayi1, sayi2));
+  print(kare(sayi3));
+  print(selamla(isim));
+}
